7430a761436923da82bfce9bd26b4a9c6dd4cd70
faizanmalik <faizan200malik@gmail.com>
2024-01-17 02:38:18 +0500

    Your commit message

--- src/components/SocialButtons.jsx
+++ src/components/SocialButtons.jsx
@@ -7,13 +7,13 @@ const SocialButtons = () => {
         <img src="src/assets/icons/fb.png" alt="Facebook" className="w-5 h-5 object-cover" />
       </a>
       <a href="https://www.instagram.com/f._.malik?igsh=dWs5dDN5bGhsdmxs&utm_source=qr" target="_blank" rel="noopener noreferrer" className="bg-pink-500 text-white rounded-full p-2 hover:bg-pink-600">
-        <img src="src\assets\icons\instagram.png" alt="Instagram" className="w-5 h-5 object-cover" />
+        <img src="src/assets/icons/instagram.png" alt="Instagram" className="w-5 h-5 object-cover" />
       </a>
       <a href="YOUR_LINKEDIN_LINK" target="_blank" rel="noopener noreferrer" className="bg-blue-600 text-white rounded-full p-2 hover:bg-blue-700">
-        <img src="src\assets\icons\linkedin.png" alt="LinkedIn" className="w-5 h-5 object-cover" />
+        <img src="src/assets/icons/linkedin.png" alt="LinkedIn" className="w-5 h-5 object-cover" />
       </a>
       <a href="https://github.com/faizanmalik2023" target="_blank" rel="noopener noreferrer" className="bg-gray-600 text-white rounded-full p-2 hover:bg-gray-700">
-        <img src="src\assets\icons\github.png" alt="GitHub" className="w-5 h-5 object-cover" />
+        <img src="src/assets/icons/github.png" alt="GitHub" className="w-5 h-5 object-cover" />
       </a>
     </div>
   );
